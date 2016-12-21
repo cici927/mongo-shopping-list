6d2569856bdb14a33b9c2514306aaf9467d5cdc2
Celia <cici_927@hotmail.com>
2016-12-21 22:43:41 +0000

    updated config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
+                            'mongodb://cici927_database:673uuzjd35@ds053251.mlab.com:53251/db-shopping-list' :
                             'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
--- test/test-server.js
+++ test/test-server.js
@@ -33,7 +33,6 @@ describe('Shopping List', function() {
         chai.request(app)
             .get('/items')
             .end(function(err, res) {
-                console.log(res.body);
                 id = res.body[0]._id;
                 should.equal(err, null);
                 res.should.have.status(200);
@@ -47,6 +46,56 @@ describe('Shopping List', function() {
                 res.body[0].name.should.equal('Broad beans');
                 res.body[1].name.should.equal('Tomatoes');
                 res.body[2].name.should.equal('Peppers');
+                done();
+            });
+    });
+    it('should add an item on POST', function(done) {
+        chai.request(app)
+            .post('/items')
+            .send({
+                'name': 'Kale'
+            })
+            .end(function(err, res) {
+                should.equal(err, null);
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.have.property('name');
+                res.body.should.have.property('_id');
+                res.body.name.should.be.a('string');
+                res.body.name.should.equal('Kale');
+                done();
+            });
+    });
+
+    it('should edit an item on PUT', function(done) {
+        chai.request(app)
+            .put('/items/' + _id)
+            .send({
+                'name': 'Eggs'
+            })
+            .end(function(err, res) {
+                should.equal(err, null);
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.an.Array;
+                res.body.should.have.property('name');
+                res.body.name.should.be.a('string');
+                res.body.name.should.equal('Eggs');
+                done();
+            });
+    });
+
+    it('should delete an item on delete', function(done) {
+        chai.request(app)
+            .delete('/items/' + _id)
+            .end(function(err, res) {
+                should.equal(err, null);
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.have.property('ok');
+                res.body.ok.should.be.a('number');
+                res.body.ok.should.equal(1);
                 done();
             });
     });
